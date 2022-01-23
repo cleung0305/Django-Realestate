a1418fadbaec81b753bd788a8c9df3460fa23ea8
Lok <loklok12614@gmail.com>
2022-01-23 00:29:40 -0500

    s_key

--- realestate/settings.py
+++ realestate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .s_key import key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hnrwva&!cs1g^s#^fc42tt&h(xhqs%*nl5*i8ttlarydiyxmx6'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
