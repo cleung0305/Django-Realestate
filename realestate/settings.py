145eb4d9d00f3220c30e737279b606a196e16c93
Lok <loklok12614@gmail.com>
2022-02-22 14:35:18 -0500

    added local settings import

--- realestate/settings.py
+++ realestate/settings.py
@@ -161,3 +161,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = email
 EMAIL_HOST_PASSWORD = email_pw  
+
+
+try:
+    from .local_settings.py import *
+except ImportError:
+    pass
